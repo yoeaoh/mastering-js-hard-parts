22b88c828acb9d02eec761eadf9288437911a65f
yoeaoh <yoeaoh@gmail.com>
2024-07-28 22:30:41 +0300

    start callbacks1

---
+++ exercises/callbacks1.js
@@ -0,0 +1,38 @@
+// Exercise 1
+// Create a function addTwo that accepts one input and adds 2 to it.
+
+function addTwo(number) {
+    return number + 2
+}
+
+// Exercise 2
+// Create a function addS that accepts one input and adds an "s" to it.
+
+// Exercise 3
+// Create a function called map that takes two inputs:
+// an array of numbers (a list of numbers)
+// a 'callback' function - a function that is applied to 
+// each element of the array (inside of the function 'map')
+// Have map return a new array filled with numbers that 
+// are the result of using the 'callback' function on each 
+// element of the input array.
+
+// Exercise 4
+// The function forEach takes an array and a callback, and 
+// runs the callback on each element of the array. forEach 
+// does not return anything.
+
+// Exercise 5
+// Rebuild your map function, this time instead of using a 
+// for loop, use your own forEach function that you just defined. 
+// Call this new function mapWith.
+
+// Exercise 6
+// The function reduce takes an array and reduces the elements 
+// to a single value. For example it can sum all the numbers, 
+// multiply them, or any operation that you can put into a function.
+
+// Exercise 7
+// Construct a function intersection that compares input arrays 
+// and returns a new array with elements found in all of the inputs. 
+// BONUS: Use reduce!
