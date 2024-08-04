b77af921a08c3670934d8c2337344a8492714071
yoeaoh <yoeaoh@gmail.com>
2024-08-04 06:08:06 +0300

    done with exercises 8 and 9

--- exercises/callbacks1.js
+++ exercises/callbacks1.js
@@ -1,20 +1,20 @@
 const testArray = [1, 2, 3, 4, 5]
 
-// Exercise 1
+// -> Exercise 1
 // Create a function addTwo that accepts one input and adds 2 to it.
 
 function addTwo(number) {
     return number + 2
 }
 
-// Exercise 2
+// -> Exercise 2
 // Create a function addS that accepts one input and adds an "s" to it.
 
 function addS(string) {
     return string + 's'
 }
 
-// Exercise 3
+// -> Exercise 3
 // Create a function called map that takes two inputs:
 // an array of numbers (a list of numbers)
 // a 'callback' function - a function that is applied to 
@@ -33,7 +33,7 @@ function customMap(array, callback) {
     return newArray
 }
 
-// Exercise 4
+// -> Exercise 4
 // The function forEach takes an array and a callback, and 
 // runs the callback on each element of the array. forEach 
 // does not return anything.
@@ -44,7 +44,7 @@ function customForEach(array, callback) {
     }
 }
 
-// Exercise 5
+// -> Exercise 5
 // Rebuild your map function, this time instead of using a 
 // for loop, use your own forEach function that you just defined. 
 // Call this new function mapWith.
@@ -59,7 +59,7 @@ function mapWith(array, callback) {
     return newArray
 }
 
-// Exercise 6
+// -> Exercise 6
 // The function reduce takes an array and reduces the elements 
 // to a single value. For example it can sum all the numbers, 
 // multiply them, or any operation that you can put into a function.
@@ -72,7 +72,7 @@ function customReduce(array, callback, initialValue) {
     return result
 }
 
-// Exercise 7
+// -> Exercise 7
 // Construct a function intersection that compares input arrays 
 // and returns a new array with elements found in all of the inputs. 
 // BONUS: Use reduce!
@@ -84,31 +84,35 @@ function intersection(...arrays) {
     }, [])
 }
 
-// Exercise 1 call
+
+
+// === execution ===
+
+// Exercise 1
 console.log('Exercise 1', addTwo(5))
 console.log('=====')
 
-// Exercise 2 call
+// Exercise 2
 console.log('Exercise 2', addS('cat'))
 console.log('=====')
 
-// Exercise 3 call
+// Exercise 3
 console.log('Exercise 3', customMap(testArray, (item) => item * 2))
 console.log('=====')
 
-// Exercise 4 call
+// Exercise 4
 console.log('Exercise 4', customForEach(testArray, (item) => item * 2))
 console.log('=====')
 
-// Exercise 5 call
+// Exercise 5
 console.log('Exercise 5', mapWith(testArray, (item) => item * 2))
 console.log('=====')
 
-// Exercise 6 call
+// Exercise 6
 console.log('Exercise 6', customReduce(testArray, (item1, item2) => item1 + item2, 0))
 console.log('=====')
 
-// Exercise 6 call
+// Exercise 6
 console.log('Exercise 7', intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]))
 console.log('=====')
 
--- exercises/callbacks2.js
+++ exercises/callbacks2.js
@@ -1,11 +1,25 @@
-// Exercise 8
+// -> Exercise 8
 // Construct a function union that compares input arrays and returns 
 // a new array that contains all elements. If there are duplicate elements, 
 // only add it once to the new array. Preserve the order of the elements 
 // starting from the first element of the first input array. 
 // BONUS: Use reduce!
 
-// Exercise 9
+function setArrays(...arrays) {
+    return arrays.reduce((acc, current) => {
+        const newArray = acc
+
+        for (let i = 0; i < current.length; i++) {
+            if (!newArray.includes(current[i])) {
+                newArray.push(current[i])
+            }
+        }
+
+        return newArray
+    }, [])
+}
+
+// -> Exercise 9
 // Construct a function objOfMatches that accepts two arrays and a callback. 
 // objOfMatches will build an object and return it. To build the object, 
 // objOfMatches will test each element of the first array using the callback 
@@ -14,16 +28,46 @@
 // a key in an object, and the element from the second array becomes 
 // the corresponding value.
 
-// Exercise 10
+function objOfMatches(arrayOne, arrayTwo, callback) {
+    const resultObject = {}
+
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (callback(arrayOne[i]) === arrayTwo[i]) {
+            resultObject[arrayOne[i]] = arrayTwo[i]
+        }
+    }
+
+    return resultObject
+}
+
+// -> Exercise 10
 // Construct a function multiMap that will accept two arrays: an array of 
 // values and an array of callbacks. multiMap will return an object whose keys 
 // match the elements in the array of values. The corresponding values that are 
 // assigned to the keys will be arrays consisting of outputs from the array of 
 // callbacks, where the input to each callback is the key.
 
-// Exercise 11
+// -> Exercise 11
 // Construct a function objectFilter that accepts an object as the first 
 // parameter and a callback function as the second parameter. objectFilter will 
 // return a new object. The new object will contain only the properties from 
 // the input object such that the property's value is equal to the property's 
 // key passed into the callback.
+
+
+
+// === execution ===
+
+// Exercise 8
+console.log(setArrays([1,2,3], [3,4,5,6,6], [1,2,3,6,6,7,7,7,8,9,9,9]))
+
+// Exercise 9
+console.log(
+    objOfMatches(
+      ["hi", "bye", "later", "hello"], // hello dont match after uppercasing
+      ["HI", "BYE", "LATER", "hello"],
+      function (str) {
+        return str.toUpperCase();
+      }
+    )
+);
